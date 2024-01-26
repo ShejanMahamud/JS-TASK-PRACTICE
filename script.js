ac0a95b3c213c60dacff73dbec267e19699578d3
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-01-26 12:58:09 +0600

    push to git

---
+++ script.js
@@ -0,0 +1,38 @@
+// let moneyHave = parseFloat(prompt("How much money you have?"));
+// let applePrice = 400;
+// let orangePrice =700;
+// if(applePrice+orangePrice>moneyHave){
+//     document.write("You need more: "+ ((orangePrice+applePrice)-moneyHave) +"Taka to buy apple and oranges")
+// }else{
+//     document.write("Price of Apple is: "+applePrice+" and "+"Price of orange is: "+orangePrice+ " You gave me: "+moneyHave+" You will get back: "+ (moneyHave-(applePrice+orangePrice)) + "Taka");
+// }
+
+
+// Problem 2
+
+// let bangla = parseFloat(prompt("Enter bangla mark: "));
+// let english = parseFloat(prompt("Enter english mark: "));
+// let math = parseFloat(prompt("Enter math mark"));
+// let biology = parseFloat(prompt("Enter biology mark"));
+// let physics = parseFloat(prompt("Enter physics mark"));
+// let programming = parseFloat(prompt("Enter programming mark: "))
+
+// let totalMark = bangla+english+math+biology+physics+programming;
+
+// if(totalMark<400){
+//     document.write("You're failed");
+// }else{
+//     document.write("Your total avarage mark: "+(totalMark/6).toFixed(2));
+// }
+
+
+// Problem 3
+
+// let userInput= parseFloat(prompt("Enter a number: "));
+// let modulus= userInput%5;
+// document.write("After dividing by 5 the remainder is: " + modulus);
+
+// problem 4
+
+var a = isNaN(10-2);
+console.log(a);
